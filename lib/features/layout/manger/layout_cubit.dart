6d44b5b7e041925f7aacd547a8337db3c40dbf10
elmosalamy <33047490.ahmei@gmail.com>
2023-08-31 00:22:48 +0300

    refactor layout of app

---
+++ lib/features/layout/manger/layout_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../home/presentation/views/home_view.dart';
+import '../../notification/presentation/views/notifications _view.dart';
+import '../../order/presentation/views/order_view.dart';
+import '../../settings/presentation/views/settings_view.dart';
+
+part 'layout_state.dart';
+
+class LayoutCubit extends Cubit<LayoutState> {
+  LayoutCubit() : super(LayoutInitial());
+
+  static LayoutCubit get(context){
+    return BlocProvider.of(context);
+  }
+
+  int currentIndex=0;
+  void changeBottomScreen(index){
+    currentIndex=index;
+    emit(ChangeBottomScreen());
+  }
+  List<Widget>bottomScreen=const [HomeView(),NotificationsView(),OrderView(),SettingsView()];
+}
