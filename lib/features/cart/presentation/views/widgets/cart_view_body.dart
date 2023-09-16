9ca5dc2ba37ed4a54cce71e026ebe6dfc450f566
khalidgamal0 <khalidgamal7773@gmail.com>
2023-09-17 00:36:36 +0300

    delete order view

--- lib/features/cart/presentation/views/widgets/cart_view_body.dart
+++ lib/features/cart/presentation/views/widgets/cart_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class OrderViewBody extends StatelessWidget {
-  const OrderViewBody({Key? key}) : super(key: key);
+class CartViewBody extends StatelessWidget {
+  const CartViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
